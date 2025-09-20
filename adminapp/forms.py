3a3f9e44659213cca82b3183b8e2305abc1ab2bc
MamurTurdikulov <turdikulovmamur@gmail.com>
2025-09-20 17:11:10 +0300

    Initial commit with all files and folders

--- adminapp/forms.py
+++ adminapp/forms.py
@@ -1,3 +1,52 @@
 from django import forms
 from .models import *
-from . import services
+
+
+class FacultyForm(forms.ModelForm):
+    class Meta:
+        model = Faculty
+        fields = "__all__"
+        widgets = {
+            "name": forms.TextInput(attrs={'class': 'form-control'})
+        }
+
+
+class KafedraForm(forms.ModelForm):
+    class Meta:
+        model = Kafedra
+        fields = "__all__"
+        widgets = {
+            "name": forms.TextInput(attrs={'class': 'form-control'})
+        }
+
+class SubjectForm(forms.ModelForm):
+    class Meta:
+        model = Subject
+        fields = "__all__"
+        widgets = {
+            "name": forms.TextInput(attrs={'class': 'form-control'})
+        }
+
+class TeacherForm(forms.ModelForm):
+    class Meta:
+        model=Teacher
+        fields="__all__"
+        widgets={
+            "name":forms.TextInput(attrs={'class':'form-control'})
+        }
+
+class GroupForm(forms.ModelForm):
+    class Meta:
+        model=Groups
+        fields="__all__"
+        widgets={
+            "name":forms.TextInput(attrs={'class':'form-control'})
+        }
+
+class StudentForm(forms.ModelForm):
+    class Meta:
+        model=Students
+        fields="__all__"
+        widgets={
+            "name":forms.TextInput(attrs={'class':'form-control'})
+        }
---
+++ adminapp/services.py
@@ -0,0 +1,55 @@
+from django.db import connection
+from contextlib import closing
+
+
+def dictfetchall(cursor):
+    columns = [col[0] for col in cursor.description]
+    return [
+        dict(zip(columns, row)) for row in cursor.fetchall()
+    ]
+
+
+def dictfetchone(cursor):
+    row = cursor.fetchone()
+    if row is None:
+        return False
+    columns = [col[0] for col in cursor.description]
+    return dict(zip(columns, row))
+
+
+def get_faculties():
+    with closing(connection.cursor()) as cursor:
+        cursor.execute("""SELECT * from adminapp_faculty""")
+        faculties = dictfetchall(cursor)
+        return faculties
+
+
+def get_kafedra():
+    with closing(connection.cursor()) as cursor:
+        cursor.execute("""SELECT * from adminapp_kafedra""")
+        kafedra = dictfetchall(cursor)
+        return kafedra
+
+def get_subjects():
+    with closing(connection.cursor()) as cursor:
+        cursor.execute("""SELECT * from adminapp_subject""")
+        subject=dictfetchall(cursor)
+        return subject
+
+def get_teachers():
+    with closing(connection.cursor()) as cursor:
+        cursor.execute("""SELECT * from adminapp_teacher""")
+        teachers=dictfetchall(cursor)
+        return teachers
+
+def get_groups():
+    with closing(connection.cursor()) as cursor:
+        cursor.execute("""SELECT * from adminapp_groups""")
+        groups=dictfetchall(cursor)
+        return groups
+
+def get_students():
+    with closing(connection.cursor()) as cursor:
+        cursor.execute("""SELECT * from adminapp_students""")
+        students=dictfetchall(cursor)
+        return students
